e108ba75f2b2feb4e17fc21a0dca167cc68fe347
Durgesh urkude <php@inhpl.com>
2026-05-09 13:04:18 +0530

    mail funality chnages

--- config/auth.php
+++ config/auth.php
@@ -19,6 +19,7 @@ return [
         'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
+    
 
     /*
     |--------------------------------------------------------------------------
