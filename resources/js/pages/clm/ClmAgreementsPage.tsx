5b57fd575878423a36b8afbc17dd046305bdadd0
react-sakshi <react@inhpl.com>
2026-06-13 14:35:47 +0530

    table pagination chnages

--- resources/js/pages/clm/ClmAgreementsPage.tsx
+++ resources/js/pages/clm/ClmAgreementsPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from 'react';
+import WorklistPager from "../../components/ui/WorklistPager";
 import { createPortal } from 'react-dom';
 import api from '../../api';
 import { useToast } from '../../contexts/ToastContext';
@@ -192,14 +193,7 @@ function TypesPane({ rows, loading, reload }: { rows: AgrType[]; loading: boolea
               </tbody>
             </table>
             {!loading && filtered.length > 0 && (
-              <div className="clm-pag">
-                <span className="clm-pag-info">Showing <b>{start + 1}–{start + slice.length}</b> of <b>{filtered.length}</b></span>
-                <div className="clm-pag-btns">
-                  {Array.from({ length: pageCount }, (_, i) => i + 1).map(p => (
-                    <button key={p} onClick={() => setPage(p)} disabled={p === safePage} className={`clm-pag-btn ${p === safePage ? 'on' : ''}`}>{p}</button>
-                  ))}
-                </div>
-              </div>
+              <WorklistPager total={filtered.length} page={safePage} pageSize={rpp} onPage={setPage} />
             )}
           </div>
         )}
@@ -404,14 +398,7 @@ function LibraryPane({ rows, types, segs, loading, reload }: { rows: AgrLib[]; t
               </tbody>
             </table>
             {!loading && filtered.length > 0 && (
-              <div className="clm-pag">
-                <span className="clm-pag-info">Showing <b>{start + 1}–{start + slice.length}</b> of <b>{filtered.length}</b></span>
-                <div className="clm-pag-btns">
-                  {Array.from({ length: pageCount }, (_, i) => i + 1).map(p => (
-                    <button key={p} onClick={() => setPage(p)} disabled={p === safePage} className={`clm-pag-btn ${p === safePage ? 'on' : ''}`}>{p}</button>
-                  ))}
-                </div>
-              </div>
+              <WorklistPager total={filtered.length} page={safePage} pageSize={rpp} onPage={setPage} />
             )}
           </div>
         )}
--- resources/js/pages/clm/ClmTncPage.tsx
+++ resources/js/pages/clm/ClmTncPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from 'react';
+import WorklistPager from "../../components/ui/WorklistPager";
 import { createPortal } from 'react-dom';
 import api from '../../api';
 import { useToast } from '../../contexts/ToastContext';
@@ -182,14 +183,7 @@ function CategoriesPane({ rows, loading }: { rows: Cat[]; loading: boolean; relo
               </tbody>
             </table>
             {!loading && filtered.length > 0 && (
-              <div className="clm-pag">
-                <span className="clm-pag-info">Showing <b>{start + 1}–{start + slice.length}</b> of <b>{filtered.length}</b></span>
-                <div className="clm-pag-btns">
-                  {Array.from({ length: pageCount }, (_, i) => i + 1).map(p => (
-                    <button key={p} onClick={() => setPage(p)} disabled={p === safePage} className={`clm-pag-btn ${p === safePage ? 'on' : ''}`}>{p}</button>
-                  ))}
-                </div>
-              </div>
+              <WorklistPager total={filtered.length} page={safePage} pageSize={rpp} onPage={setPage} />
             )}
           </div>
         )}
@@ -342,14 +336,7 @@ function LibraryPane({ rows, cats, segs, loading, reload }: { rows: Lib[]; cats:
               </tbody>
             </table>
             {!loading && filtered.length > 0 && (
-              <div className="clm-pag">
-                <span className="clm-pag-info">Showing <b>{start + 1}–{start + slice.length}</b> of <b>{filtered.length}</b></span>
-                <div className="clm-pag-btns">
-                  {Array.from({ length: pageCount }, (_, i) => i + 1).map(p => (
-                    <button key={p} onClick={() => setPage(p)} disabled={p === safePage} className={`clm-pag-btn ${p === safePage ? 'on' : ''}`}>{p}</button>
-                  ))}
-                </div>
-              </div>
+              <WorklistPager total={filtered.length} page={safePage} pageSize={rpp} onPage={setPage} />
             )}
           </div>
         )}
